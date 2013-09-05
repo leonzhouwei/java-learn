60176029a890978413ed2e18d7608792392b1d18
zhouwei <zhouwei.zw@alibaba-inc.com>
2013-09-05 21:36:10 +0800

    logback can log out every minute and package the log file

--- logback-demo/src/main/java/edu/xidian/sei/grade2011/zhouwei/java/learn/logback/App.java
+++ logback-demo/src/main/java/edu/xidian/sei/grade2011/zhouwei/java/learn/logback/App.java
@@ -6,8 +6,11 @@ import org.slf4j.LoggerFactory;
 public class App {
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
 	
-	public static void main(String[] args) {
-		logger.debug("debug");
-		logger.info("info");
+	public static void main(String[] args) throws InterruptedException {
+		while (true) {
+			logger.debug("debug");
+			logger.info("info");
+			Thread.sleep(60000);
+		}
 	}
 }
